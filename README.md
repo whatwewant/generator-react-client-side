5b82e9e6c7d5243774f76a87ea40f853e9b73d82
whatwewant <tobewhatwewant@gmail.com>
2016-09-20 04:50:22 +0800

    react client side generator @v1.0.0

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-react-client-side [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Eason React Client Side Generator
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-react-client-side using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-react-client-side
+```
+
+Then generate your new project:
+
+```bash
+yo react-client-side
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Eason Smith](http://colesmith.space)
+
+
+[npm-image]: https://badge.fury.io/js/generator-react-client-side.svg
+[npm-url]: https://npmjs.org/package/generator-react-client-side
+[travis-image]: https://travis-ci.org/whatwewant/generator-react-client-side.svg?branch=master
+[travis-url]: https://travis-ci.org/whatwewant/generator-react-client-side
+[daviddm-image]: https://david-dm.org/whatwewant/generator-react-client-side.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/whatwewant/generator-react-client-side
+[coveralls-image]: https://coveralls.io/repos/whatwewant/generator-react-client-side/badge.svg
+[coveralls-url]: https://coveralls.io/r/whatwewant/generator-react-client-side
---
+++ generators/app/index.js
@@ -0,0 +1,145 @@
+'use strict';
+const yeoman = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require('path');
+const os = require('os');
+const _ = require('lodash');
+const extend = require('deep-extend');
+const mkdirp = require('mkdirp');
+
+const USERNAME = os.userInfo().username
+
+module.exports = yeoman.Base.extend({
+  initializing () {
+    this.props = {};
+  },
+
+  prompting () {
+    this.log(yosay(
+      'Welcome to the mathematical ' + chalk.red('generator-react-client-side') + ' generator!'
+    ));
+
+    const prompts = [{
+      type: 'input',
+      name: 'projectName',
+      message: 'Input Project Name:',
+    }, {
+      type: 'input',
+      name: 'projectDesc',
+      message: 'Input Project Description:',
+    }, {
+      type: 'confirm',
+      name: 'projectMain',
+      message: 'Main File (app.js):',
+      default: true
+    }, {
+      type: 'input',
+      name: 'projectAuthor',
+      message: `Author (Default: ${ USERNAME }):`,
+      default: USERNAME
+    }, {
+      type: 'list',
+      name: 'projectLicense',
+      message: 'Choose License (Default: MIT):',
+      choices: ['MIT', 'ISC', 'Apache-2.0', 'AGPL-3.0'],
+      default: 'MIT',
+    }];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  },
+
+  defaults () {
+    if (path.basename(this.destinationPath()) !== this.props.projectName) {
+      this.log(
+        `You generator must be inside a folder named ${this.props.projectName} I will automatically create this folder.`
+      );
+    }
+
+    mkdirp(this.props.projectName);
+    this.destinationRoot(this.destinationPath(this.props.projectName));
+  },
+
+  writing () {
+    // File Tree
+    //    .
+    //    ├── package.json
+    //    ├── src
+    //    │   ├── actions/
+    //    │   ├── app.js
+    //    │   ├── components/
+    //    │   ├── containers/
+    //    │   ├── index.html
+    //    │   ├── reducers/
+    //    │   ├── store/
+    //    │   └── lib/
+    //    └── webpack.config.js
+
+
+    // 1 package.json
+    const defaultPkg = this.fs.readJSON(this.templatePath('package.json'), {});
+    const pkg = extend(
+      {},
+      defaultPkg,
+      {
+        name: this.props.projectName,
+        description: this.props.projectDesc,
+        main: this.props.projectMain,
+        author: this.props.projectAuthor,
+        license: this.props.projectLicense,
+        keywords: [ ...defaultPkg.keywords, 'generator-react-client-side' ],
+        devDependencies: {
+          // develop dependencies
+        },
+        dependencies: {
+          // production dependencies
+        }
+      }
+    );
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+
+    // 2 webpack config
+    this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
+
+    // 3 .gitignore
+    this.fs.copy(
+      this.templatePath('.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    // 4 directories
+    mkdirp('src/components');
+    mkdirp('src/containers');
+    mkdirp('src/actions');
+    mkdirp('src/reducers');
+    mkdirp('src/store');
+    mkdirp('src/lib');
+
+    // 5 index.html
+    this.fs.copy(
+      this.templatePath('index.html'),
+      'src/index.html'
+    );
+
+    // 6 entry: app.js
+    this.fs.copy(
+      this.templatePath('app.js'),
+      `src/app.js`
+    );
+
+    // 7 License
+    this.fs.copy(
+      this.templatePath('LICENSE'),
+      this.destinationPath('LICENSE')
+    );
+  },
+
+  install () {
+    this.installDependencies();
+  }
+});
